55018538471c084d50547641b30c36a6bdcfa66c
ephrem <yilimaepho@gmail.com>
2021-06-23 01:12:21 -0400

    this is employess name

---
+++ ephrem-java1/src/comm9/EmloyesName.java
@@ -0,0 +1,12 @@
+package comm9;
+
+public class EmloyesName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String firstname = "Ephrem";
+		String lastname = "begashaw";
+System.out.println("Employees Full Name is :" + firstname + " " + lastname);
+
+}
+}
