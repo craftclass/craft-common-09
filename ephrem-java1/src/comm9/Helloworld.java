b1af0c9dacd818dfd04f3b21de785570b04987e9
ephrem <yilimaepho@gmail.com>
2021-06-22 20:47:27 -0400

    commit Hello world message

---
+++ ephrem-java1/src/comm9/Helloworld.java
@@ -0,0 +1,11 @@
+package comm9;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello world from ephrem brach1");
+	}
+
+}
