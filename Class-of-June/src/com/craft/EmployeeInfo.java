58c532b2e779633d95aec743d674085e61367903
negussiemekonnen <negussiea27@gmail.com>
2021-06-25 10:14:14 -0400

    updated

--- Class-of-June/src/com/craft/EmployeeInfo.java
+++ Class-of-June/src/com/craft/EmployeeInfo.java
@@ -4,6 +4,12 @@ public class EmployeeInfo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+         String firstname = "Negussie";
+         String lastname  = "Mekonnen";
+         
+         System.out.println("Employee fullname: " + firstname +  " " + lastname);
+         
 
 	}
 
